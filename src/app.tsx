a4f4ad5a2986c0ad110ba5726adb814ee617c4ed
huhguz <huhguz@mail.ru>
2020-10-12 19:44:59 +0300

    components

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,27 @@
 import React, { useState, FunctionComponent } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { DatePicker } from 'antd';
+import { DatePicker, Button, Pagination, Select } from 'antd';
+import fs from 'fs';
 import ClicksShower from './components/ClicksShower';
 import 'antd/dist/antd.less';
 
 const App: FunctionComponent = () => {
+    const [opened, setOpened] = useState(true);
+    console.log(opened);
+
+    const onClick = () => {
+        setOpened((prev) => !prev);
+    };
+
     return (
         <div>
             <div />
             test app
             <ClicksShower counterClickStore="sds" />
-            <DatePicker />
+            <DatePicker disabled={opened} />
+            <Button onClick={onClick}>Нажми меня</Button>
+            <Pagination disabled={opened} total={50} showSizeChanger />
+            <Select disabled={opened} />
         </div>
     );
 };
